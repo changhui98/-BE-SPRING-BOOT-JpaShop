3f0173a6a048c30d303e25422dee3e0aaef09131
changhui98 <net_0102@naver.com>
2025-05-02 16:46:37 +0900

    [BE] 회원 서비스 로직 구현

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,22 +3,23 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
 
         em.persist(member);
     }
 
-    public Member member(Long id) {
+    public Member findOne(Long id) {
 
         return em.find(Member.class, id);
     }
